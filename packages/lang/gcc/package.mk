49fbd59d80c21eeb07540ef2415414505c417eab
Alex Deryskyba <alex@codesnake.com>
2014-05-25 13:05:14 +0300

    Add support for Amlogic G18 MX2 devices.
    Conflicts:
    
    	packages/lang/gcc/package.mk
    	packages/linux/package.mk

--- packages/lang/gcc/package.mk
+++ packages/lang/gcc/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="gcc"
-PKG_VERSION="4.9.0"
+PKG_VERSION="4.7.3"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
