f5bb927aae4cf479fb1fe10450821385c8b8ba13
Alex Deryskyba <alex@codesnake.com>
2015-04-23 14:39:48 +0200

    [aml] Update Linux kernel to 3.10-a2f8d73

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -19,7 +19,7 @@
 PKG_NAME="linux"
 case "$LINUX" in
   amlogic)
-    LINUX_VERSION="3.10-d0ab478"
+    LINUX_VERSION="3.10-a2f8d73"
     PKG_VERSION="amlogic-$LINUX_VERSION"
     PKG_URL="$DISTRO_SRC/$PKG_NAME-$PKG_VERSION.tar.xz"
     ;;
